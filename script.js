470a3407fe9c38037afd3b656162508839e89899
Asparagaliz Lily <asparagaliz@gmail.com>
2018-06-27 19:43:18 +0800

    svg resize

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ function b64DecodeUnicode(str) {
 	}).join(''));
 }
 
+function setSVGsize(id, width, height) {
+	sizeScale = document.body.clientHeight / height
+	sizeTransformX = (document.body.clientWidth - width*sizeScale)/sizeScale
+	sizeTransformY = document.body.clientHeight * 0.05
+	svg = document.getElementById(id)
+	svg.setAttribute("transform",`scale(${sizeScale}) translate(${sizeTransformX}, ${sizeTransformY})`)
+}
+
 theTales = ['5p+Q5YCL5pmC56m66KOP55qEIEFzcGFyYWdhbGl6IOaIkOmVt+WcqOWNl+Wci+eahOemj+WIqemZou+8jOWcqCA3IOatsuaZguiiq+mkiueItuavjeW4tuWbnuWutu+8jOWcqCAxMyDmrbLnmoTmmYLlgJnlm6DngrrlnLDkuIvpkLXmspnmnpfkuovku7blpLHljrvkuobppIrniLbmr43vvIzmraTlvozkuIDnm7Tnjajoh6rmtYHmtarjgII=',
 '5p+Q5YCL5pmC56m66KOP55qEIEFzcGFyYWdhbGl6IOWcqOS4gOmWk+WQjeeCuuOAjOiZjuWwvuiNieOAjeeahOWSluWVoeW7s+mHjOeVtuWls+WDle+8jOS4puaChOaChOeahOWWnOatoeavj+WkqeaUvuWtuOaZgumWk+mDveacg+S+huioqueahOepv+mdm+iXjeiJsuaWueagvOeZvuiktuijmeeahOWls+WtqeWtkOOAgg==',
 '5p+Q5YCL5pmC56m66KOP55qEIEFzcGFyYWdhbGl6IOacg+S4gOeoruiuk+iHquW3seeci+i1t+S+hui6q+mrmOiuiumrmOeahOmtlOazle+8jOmAmeiuk+WlueiDveWkoOecgeS4i+iyt+mrmOi3n+mei+eahOmMoueUqOaWvOeNqOiHquS6q+eUqOaxn+axkOWFrOWckueahOWPr+m6l+mkheOAgg==',
@@ -20,3 +28,12 @@ theTales = ['5p+Q5YCL5pmC56m66KOP55qEIEFzcGFyYWdhbGl6IOaIkOmVt+WcqOWNl+Wci+eahOe
 aTaleText = b64DecodeUnicode(theTales[Math.floor(Math.random()*theTales.length)])
 document.getElementById('a-tale').innerText = aTaleText
 document.getElementById('a-tale-shadow').innerText = aTaleText
+
+function setSVGsizeSets() {
+	setSVGsize("aspa1",528.93,637.25)
+}
+window.addEventListener('resize', setSVGsizeSets)
+setSVGsizeSets()
+
+svgRandoms = ["svg-aspa1", "", "", "", ""]
+document.getElementsByTagName("body")[0].classList += svgRandoms[Math.floor(Math.random()*svgRandoms.length)]
